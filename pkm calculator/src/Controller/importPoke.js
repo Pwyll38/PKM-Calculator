a42dfc501f5fc9be935a8cd96c4135bb840188f9
pwyll <diascamara38@gmail.com>
2025-09-01 20:45:58 -0300

    first commit

---
+++ pkm calculator/src/Controller/importPoke.js
@@ -0,0 +1,368 @@
+import calc from '@smogon/calc'
+
+function ExportPokemon(pokeInfo) {
+    var pokemon = createPokemon(pokeInfo);
+    var EV_counter = 0;
+    var finalText = "";
+    finalText = pokemon.name + (pokemon.item ? " @ " + pokemon.item : "") + "\n";
+    finalText += "Level: " + pokemon.level + "\n";
+    finalText += pokemon.nature && gen > 2 ? pokemon.nature + " Nature" + "\n" : "";
+    if (gen === 9) {
+        var teraType = pokeInfo.find(".teraType").val();
+        if (teraType !== undefined && teraType !== pokemon.types[0]) {
+            finalText += "Tera Type: " + teraType + "\n";
+        }
+    }
+    finalText += pokemon.ability ? "Ability: " + pokemon.ability + "\n" : "";
+    if (gen > 2) {
+        var EVs_Array = [];
+        for (var stat in pokemon.evs) {
+            var ev = pokemon.evs[stat] ? pokemon.evs[stat] : 0;
+            if (ev > 0) {
+                EVs_Array.push(ev + " " + calc.Stats.displayStat(stat));
+            }
+            EV_counter += ev;
+            if (EV_counter > 510) break;
+        }
+        if (EVs_Array.length > 0) {
+            finalText += "EVs: ";
+            finalText += serialize(EVs_Array, " / ");
+            finalText += "\n";
+        }
+    }
+
+    var IVs_Array = [];
+    for (var stat in pokemon.ivs) {
+        var iv = pokemon.ivs[stat] ? pokemon.ivs[stat] : 0;
+        if (iv < 31) {
+            IVs_Array.push(iv + " " + calc.Stats.displayStat(stat));
+        }
+    }
+    if (IVs_Array.length > 0) {
+        finalText += "IVs: ";
+        finalText += serialize(IVs_Array, " / ");
+        finalText += "\n";
+    }
+
+    for (var i = 0; i < 4; i++) {
+        var moveName = pokemon.moves[i].name;
+        if (moveName !== "(No Move)") {
+            finalText += "- " + moveName + "\n";
+        }
+    }
+    finalText = finalText.trim();
+}
+
+
+function serialize(array, separator) {
+    var text = "";
+    for (var i = 0; i < array.length; i++) {
+        if (i < array.length - 1) {
+            text += array[i] + separator;
+        } else {
+            text += array[i];
+        }
+    }
+    return text;
+}
+
+function getAbility(row) {
+    var ability = row[1] ? row[1].trim() : '';
+    if (calc.ABILITIES[9].indexOf(ability) !== -1) return ability;
+}
+
+function getTeraType(row) {
+    var teraType = row[1] ? row[1].trim() : '';
+    if (Object.keys(calc.TYPE_CHART[9]).slice(1).indexOf(teraType) !== -1) return teraType;
+}
+
+function statToLegacyStat(stat) {
+    switch (stat) {
+        case 'hp':
+            return "hp";
+        case 'atk':
+            return "at";
+        case 'def':
+            return "df";
+        case 'spa':
+            return "sa";
+        case 'spd':
+            return "sd";
+        case 'spe':
+            return "sp";
+    }
+}
+
+function getStats(currentPoke, rows, offset) {
+    currentPoke.nature = "Serious";
+    var currentEV;
+    var currentIV;
+    var currentAbility;
+    var currentTeraType;
+    var currentNature;
+    currentPoke.level = 100;
+    for (var x = offset; x < offset + 9; x++) {
+        var currentRow = rows[x] ? rows[x].split(/[/:]/) : '';
+        var evs = {};
+        var ivs = {};
+        var ev;
+        var j;
+
+        switch (currentRow[0]) {
+            case 'Level':
+                currentPoke.level = parseInt(currentRow[1].trim());
+                break;
+            case 'EVs':
+                for (j = 1; j < currentRow.length; j++) {
+                    currentEV = currentRow[j].trim().split(" ");
+                    currentEV[1] = statToLegacyStat(currentEV[1].toLowerCase());
+                    evs[currentEV[1]] = parseInt(currentEV[0]);
+                }
+                currentPoke.evs = evs;
+                break;
+            case 'IVs':
+                for (j = 1; j < currentRow.length; j++) {
+                    currentIV = currentRow[j].trim().split(" ");
+                    currentIV[1] = statToLegacyStat(currentIV[1].toLowerCase());
+                    ivs[currentIV[1]] = parseInt(currentIV[0]);
+                }
+                currentPoke.ivs = ivs;
+                break;
+
+        }
+        currentAbility = rows[x] ? rows[x].trim().split(":") : '';
+        if (currentAbility[0] == "Ability") {
+            currentPoke.ability = currentAbility[1].trim();
+        }
+
+        currentTeraType = rows[x] ? rows[x].trim().split(":") : '';
+        if (currentTeraType[0] == "Tera Type") {
+            currentPoke.teraType = currentTeraType[1].trim();
+        }
+
+        currentNature = rows[x] ? rows[x].trim().split(" ") : '';
+        if (currentNature[1] == "Nature" && currentNature[0] != "-") {
+            currentPoke.nature = currentNature[0];
+        }
+    }
+    return currentPoke;
+}
+
+function getItem(currentRow, j) {
+    for (; j < currentRow.length; j++) {
+        var item = currentRow[j].trim();
+        if (calc.ITEMS[9].indexOf(item) != -1) {
+            return item;
+        }
+    }
+}
+
+function getMoves(currentPoke, rows, offset) {
+    var movesFound = false;
+    var moves = [];
+    for (var x = offset; x < offset + 12; x++) {
+        if (rows[x]) {
+            if (rows[x][0] == "-") {
+                movesFound = true;
+                var move = rows[x].substr(2, rows[x].length - 2).replace("[", "").replace("]", "").replace("  ", "");
+                moves.push(move);
+            } else {
+                if (movesFound == true) {
+                    break;
+                }
+            }
+        }
+    }
+    currentPoke.moves = moves;
+    return currentPoke;
+}
+function updateDex(customsets) {
+    for (var pokemon in customsets) {
+        for (var moveset in customsets[pokemon]) {
+            if (!SETDEX_SV[pokemon]) SETDEX_SV[pokemon] = {};
+            SETDEX_SV[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_SS[pokemon]) SETDEX_SS[pokemon] = {};
+            SETDEX_SS[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_SM[pokemon]) SETDEX_SM[pokemon] = {};
+            SETDEX_SM[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_XY[pokemon]) SETDEX_XY[pokemon] = {};
+            SETDEX_XY[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_BW[pokemon]) SETDEX_BW[pokemon] = {};
+            SETDEX_BW[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_DPP[pokemon]) SETDEX_DPP[pokemon] = {};
+            SETDEX_DPP[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_ADV[pokemon]) SETDEX_ADV[pokemon] = {};
+            SETDEX_ADV[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_GSC[pokemon]) SETDEX_GSC[pokemon] = {};
+            SETDEX_GSC[pokemon][moveset] = customsets[pokemon][moveset];
+            if (!SETDEX_RBY[pokemon]) SETDEX_RBY[pokemon] = {};
+            SETDEX_RBY[pokemon][moveset] = customsets[pokemon][moveset];
+        }
+    }
+    localStorage.customsets = JSON.stringify(customsets);
+}
+
+function addSets(pokes, name) {
+
+    var pokes = 
+
+`Beats walkin (Koraidon) @ Life Orb  
+Ability: Orichalcum Pulse  
+Level: 50  
+Tera Type: Fire  
+EVs: 252 Atk / 4 SpD / 252 Spe  
+Adamant Nature  
+- Flare Blitz  
+- Close Combat  
+- U-turn  
+- Drain Punch  
+
+Miraidon @ Choice Scarf  
+Ability: Hadron Engine  
+Level: 50  
+Tera Type: Electric  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Modest Nature  
+- Electro Drift  
+- Draco Meteor  
+- Volt Switch  
+- Solar Beam  
+
+Flutter Mane @ Choice Specs  
+Ability: Protosynthesis  
+Level: 50  
+Tera Type: Ghost  
+EVs: 252 SpA / 4 SpD / 252 Spe  
+Timid Nature  
+IVs: 0 Atk  
+- Moonblast  
+- Shadow Ball  
+- Power Gem  
+- Mystical Fire  
+
+Chien-Pao @ Focus Sash  
+Ability: Sword of Ruin  
+Level: 50  
+Tera Type: Ice  
+EVs: 252 Atk / 4 SpA / 252 Spe  
+Adamant Nature  
+- Swords Dance  
+- Sacred Sword  
+- Ice Spinner  
+- Sucker Punch  
+
+Ting-Lu @ Assault Vest  
+Ability: Vessel of Ruin  
+Level: 50  
+Tera Type: Dark  
+EVs: 252 HP / 252 Atk / 4 SpD  
+Impish Nature  
+- Payback  
+- Earthquake  
+- Heavy Slam  
+- Ruination  
+
+Garganacl @ Rocky Helmet  
+Ability: Purifying Salt  
+Level: 50  
+Tera Type: Rock  
+EVs: 252 HP / 4 Atk / 252 Def  
+Impish Nature  
+- Salt Cure  
+- Protect  
+- Earthquake  
+- Recover  
+`
+
+
+    var rows = pokes.split("\n");
+    var currentRow;
+    var currentPoke;
+    var addedpokes = 0;
+    for (var i = 0; i < rows.length; i++) {
+        currentRow = rows[i].split(/[()@]/);
+        // Skip the current row if it contains the ability As One (Spectrier / Glastrier),
+        // so that it is not treated as another distinct set.
+        if (currentRow.length > 0 && currentRow[0].includes('As One')) continue;
+        for (var j = 0; j < currentRow.length; j++) {
+            currentRow[j] = checkExeptions(currentRow[j].trim());
+            if (calc.SPECIES[9][currentRow[j].trim()] !== undefined) {
+                currentPoke = JSON.parse(JSON.stringify(calc.SPECIES[9][currentRow[j].trim()]));
+                currentPoke.name = currentRow[j].trim();
+                currentPoke.item = getItem(currentRow, j + 1);
+                if (j === 1 && currentRow[0].trim()) {
+                    currentPoke.nameProp = currentRow[0].trim();
+                } else {
+                    currentPoke.nameProp = name;
+                }
+                currentPoke.isCustomSet = true;
+                currentPoke.ability = getAbility(rows[i + 1].split(":"));
+                currentPoke.teraType = getTeraType(rows[i + 1].split(":"));
+                currentPoke = getStats(currentPoke, rows, i + 1);
+                currentPoke = getMoves(currentPoke, rows, i);
+                console.log(currentPoke);
+                
+                addedpokes++;
+            }
+        }
+    }
+    if (addedpokes > 0) {
+        console.log(
+            "Successfully imported " + addedpokes + (addedpokes === 1 ? " set" : " sets"));
+    } else {
+        console.log("No sets imported, please check your syntax and try again");
+    }
+}
+
+function checkExeptions(poke) {
+    switch (poke) {
+        case 'Aegislash':
+            poke = "Aegislash-Blade";
+            break;
+        case 'Basculin-Blue-Striped':
+            poke = "Basculin";
+            break;
+        case 'Gastrodon-East':
+            poke = "Gastrodon";
+            break;
+        case 'Mimikyu-Busted-Totem':
+            poke = "Mimikyu-Totem";
+            break;
+        case 'Mimikyu-Busted':
+            poke = "Mimikyu";
+            break;
+        case 'Pikachu-Belle':
+        case 'Pikachu-Cosplay':
+        case 'Pikachu-Libre':
+        case 'Pikachu-Original':
+        case 'Pikachu-Partner':
+        case 'Pikachu-PhD':
+        case 'Pikachu-Pop-Star':
+        case 'Pikachu-Rock-Star':
+            poke = "Pikachu";
+            break;
+        case 'Vivillon-Fancy':
+        case 'Vivillon-Pokeball':
+            poke = "Vivillon";
+            break;
+        case 'Florges-White':
+        case 'Florges-Blue':
+        case 'Florges-Orange':
+        case 'Florges-Yellow':
+            poke = "Florges";
+            break;
+        case 'Shellos-East':
+            poke = "Shellos";
+            break;
+        case 'Deerling-Summer':
+        case 'Deerling-Autumn':
+        case 'Deerling-Winter':
+            poke = "Deerling";
+            break;
+    }
+    return poke;
+
+}
+
+console.log('Starting...');
+addSets()
